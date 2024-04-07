6a3bc951761f0b64ca5f504b6197ce7d5c2f30a9
luohao <365624786@qq.com>
2024-04-07 09:30:51 +0800

    依赖方式

--- app/src/main/java/com/gxy/zkxhcommon/Main.kt
+++ app/src/main/java/com/gxy/zkxhcommon/Main.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
  * @author zhangyuxiang
  * @date 2024/4/2
  */
+
 class Main :AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
